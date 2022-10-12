9cafd0d28d586ae32efffc7f222c99e4ab5f0505
Micah Schuster <micahdschuster@gmail.com>
2022-10-12 12:48:43 -0400

    Initial Commit

---
+++ src/edu/wit/cs/comp5900/PA4a.java
@@ -0,0 +1,11 @@
+package edu.wit.cs.comp5900;
+
+// TODO: document this class
+public class PA4a {
+
+	// TODO: document this method
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+}
